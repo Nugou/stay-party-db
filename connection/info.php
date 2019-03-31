c3863c5288eb3a55195feb0c0e10cb64930aee8b
Alessandro Santos <alessandro.vncs@gmail.com>
2019-03-31 20:50:19 -0300

    Pagina de info concluida

--- connection/info.php
+++ connection/info.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aless
- * Date: 3/31/2019
- * Time: 6:59 PM
- */
+include('connection.php');
+
+if (isset($_POST['type']) == true){
+	$type = $_POST['type'];
+}else{
+	$type = "user";
+}
+$data = array();
+
+if ($type == "user"){
+
+	$sql = $pdo->prepare("select * from user_data");
+} else if ($type == "driver"){
+	$sql = $pdo->prepare("select * from driver_data");
+} else {
+	$data['type'] = "Não especificado corretamente";
+}
+$g = 0;
+if($sql-> execute()){
+	while($row = $sql->fetch(PDO::FETCH_ASSOC)){
+		foreach($row as $field => $value){
+			$data[$g][$field] = $value;
+		}
+		$g = ($g + 1);
+	}
+}
+
+if($data == []){
+	$data["result"] = "error";
+}else{
+	$data["length"] = $sql->rowCount();
+	$data["result"] = "ok";
+}
+
+echo json_encode($data);
+?>
