4b1c7cd2ee3ea729641e20838b872389e06e8e51
pauloarn <paulo.amador98@gmail.com>
2019-04-02 09:55:11 -0300

    Login generico atualizado

--- connection/login.php
+++ connection/login.php
@@ -1,14 +1,12 @@
 <?php
 include('connection.php');
 
-if(isset($_POST)){
+if(isset($_POST['type'])){
 	$email = $_POST['email'];
 	$password = $_POST['password'];
 	$type = $_POST['type'];	
 }else{
 	$json = file_get_contents('php://input');
- 
-	// decoding the received JSON and store into $obj variable.
 	$obj = json_decode($json,true);
 	$email = $obj['email'];
 	$password = $obj['password'];
@@ -46,14 +44,18 @@ if ($type == "user"){
 			$data = array("email"=>$row->email, "password"=>$row->password, "name"=>$row->name, "type"=>$type);
 		}
 	}
-} 
+} else {
+	$data['type'] = 'error';
+}
 
 
 if($data == []){
 	$data['field'] = $type . " - " . $email . " - " . $password;
-    $data["result"] = "error";
-}else{
-    $data["result"] = "ok";
+    $data["result"] = "dados não encontrado";
+}else if($data['type'] = 'error'){
+    $data["result"] = "tipo não especificado";
+} else {
+	$data["result"] = "ok";
 }
 
 echo json_encode($data);
